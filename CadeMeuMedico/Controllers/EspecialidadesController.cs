85afc7708f54d30da2b48132704f7215781149d5
Luciano Menezes Gonçalves <lucianodev@gmail.com>
2017-06-25 20:11:09 -0300

    Controles para Especialidade

---
+++ CadeMeuMedico/Controllers/EspecialidadesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CadeMeuMedico.Models;
+using System.Data.Entity;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class EspecialidadesController : Controller
+    {
+        private CadeMeuMedicoDBEntities db = new CadeMeuMedicoDBEntities();
+
+        // GET: Especialidades
+        public ActionResult Index()
+        {
+            var especialidades = db.Especialidades.ToList();
+            return View(especialidades);
+        }
+
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adicionar(Especialidades especialidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Especialidades.Add(especialidade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(especialidade);
+        }
+
+
+        public ActionResult Editar(long id)
+        {
+            Especialidades especialidade = db.Especialidades.Find(id);
+            return View(especialidade);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Especialidades especialidade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(especialidade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                
+            return View(especialidade);
+        }
+    }
+}
